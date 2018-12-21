d8c49544203677258033c0426f3d62517ce87024
nathangriotUA <45098233+nathangriotUA@users.noreply.github.com>
2018-12-21 18:25:11 +0100

    works
    works

--- Titre.cpp
+++ Titre.cpp
@@ -1,11 +1,41 @@
 #include "Titre.h"
 
-void Titre::getTitreViaScript()
-{
 
+
+Titre::Titre()
+{
 }
 
-String Titre::getTitre()
+
+Titre::~Titre()
 {
-	return this.Titre;
+}
+
+void Titre::getTitreViaScript(string fichierResultat)
+{
+
+	ifstream file;
+	string line;
+	vector<string> abstract(10);
+	string cmp1 = "Abstract";
+	file.open(fichierResultat);
+	int i = 0;
+	int j = 0;
+	while (!file.eof())
+	{
+		getline(file, line);
+			if (i==1)
+			{
+				abstract.push_back(line);
+				break;
+				//cout << "add" << i << endl;
+			}
+		
+			i++;
+	}
+	for (unsigned int z = 0; z<abstract.size(); ++z)
+	{
+		cout << abstract[z];
+	}
+	file.close();
 }
